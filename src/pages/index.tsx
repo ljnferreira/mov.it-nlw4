bd52cec814cfe5c095473cf6431cf0cec2c22a35
Lucas Ferreira <ljnferreira@gmail.com>
2021-02-27 15:51:52 -0300

    chore: added *.modules.scss file types support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from 'next/head'
+import Head from "next/head";
 
+import styles from "./Home.module.scss";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Hello World</h1>
     </div>
-  )
+  );
 }
