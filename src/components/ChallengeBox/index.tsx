6be4375a869aba508554467de69604de71f91bd8
Lucas Ferreira <ljnferreira@gmail.com>
2021-03-04 20:56:56 -0300

    feat: added contexts to the application

--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -1,25 +1,32 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import body from "../../assets/images/icons/body.svg";
+import eye from "../../assets/images/icons/eye.svg";
 import levelUP from "../../assets/images/icons/level-up.svg";
+import { ChallengesContext } from "../../contexts/ChallengesContext";
 import styles from "./ChallengeBox.module.scss";
 
 export function ChallengeBox() {
-  const [hasActiveChallenge, setHasActiveChallenge] = useState(false);
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <section className={styles.challengeBoxActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
 
           <article className={styles.challengeBoxContent}>
-            <img src={body} alt="" />
+            {activeChallenge.type === "body" && <img src={body} alt="" />}
+            {activeChallenge.type === "eye" && <img src={eye} alt="" />}
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos</p>
+            <p>{activeChallenge.description}</p>
           </article>
 
           <footer>
-            <button type="button" className={styles.challengeBoxFailedButton}>
+            <button
+              type="button"
+              className={styles.challengeBoxFailedButton}
+              onClick={resetChallenge}
+            >
               Falhei
             </button>
             <button
@@ -32,12 +39,10 @@ export function ChallengeBox() {
         </section>
       ) : (
         <div className={styles.challengeBoxNotActive}>
-          <strong>
-            Finalize um ciclo para receber desafios a serem completados
-          </strong>
+          <strong>Finalize um ciclo para ser desafiado.</strong>
           <p>
             <img src={levelUP} alt="Level Up" />
-            Avance de level completando desafios
+            Atinja um novo nivel completando desafios
           </p>
         </div>
       )}
